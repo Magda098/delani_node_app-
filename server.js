61101b9a38fe99299f6c35857aa4dd9c90fba70a
Magda098 <magjac22@gmail.com>
2022-12-12 23:45:15 +0300

    Adds refactored code for creating a server and function to wrap our server functionality so that we can export it

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 //code for our http server
 //import the http module that is shipped together with Node.js when we install it and makes it accessible through the variable http
-let http = require('http');//
+/*let http = require('http');//
 
 
 http.createServer(function(request, response){
  response.writeHead(200, {"Content-type": "text/plain"});
  response.write("Hello World");
  response.end();
-}).listen(8000);
+}).listen(8000);*/
+//refactored code
+let http = require('http');
+// A function to wrap our server functionality so that we can export it
+let start = function(){
+
+function onRequest(request, response){
+   console.log("Request received!")
+   response.writeHead(200, {"Content-type": "text/plain"});
+   response.write("Hello World");
+   response.end();
+}
+
+http.createServer(onRequest).listen(8000);
+
+console.log("Server has started.")
+}
+exports.start= start;
