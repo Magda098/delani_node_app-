ce045512ad47de7134899ccffb6d7e693adc7ba1
Magda098 <magjac22@gmail.com>
2022-12-13 00:36:08 +0300

    Adds routing details

--- server.js
+++ server.js
@@ -10,18 +10,24 @@ http.createServer(function(request, response){
 }).listen(8000);*/
 //refactored code
 let http = require('http');
+let url = require('url');
+
 // A function to wrap our server functionality so that we can export it
 let start = function(){
 
-function onRequest(request, response){
-   console.log("Request received!")
-   response.writeHead(200, {"Content-type": "text/plain"});
-   response.write("Hello World");
-   response.end();
+   function onRequest(request, response){
+       // Extracting the pathname from the url requested
+       let pathname = url.parse(request.url).pathname
+       console.log(pathname)
+       console.log("Request received!")
+       response.writeHead(200, {"Content-type": "text/plain"});
+       response.write("Hello World");
+       response.end();
+   }
+  
+   http.createServer(onRequest).listen(8000);
+  
+   console.log("Server has started.")
 }
 
-http.createServer(onRequest).listen(8000);
-
-console.log("Server has started.")
-}
-exports.start= start;
+exports.start = start;
