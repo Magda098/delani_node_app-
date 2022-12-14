43affa23f73e1622cef1ea7156b8ecdd6ccd73fc
Magda098 <magjac22@gmail.com>
2022-12-14 09:57:13 +0300

    define handle object and pass it to the server as an additional parameter

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 let server = require('./server');
 let router = require('./router');
+//mapping different URLs to the respective functions in our request handler module as key-value pairs
+//define handle object,pass it to the server as an additional parameter. 
+let requestHandlers = require("./requestHandler");
+
+let handle = {};
+handle["/"] = requestHandlers.index;
+handle["/index.html"] = requestHandlers.index;
+handle["/portfolio.html"] = requestHandlers.portfolio;
+
+server.start(router.route, handle);
 
 server.start(router.route)
